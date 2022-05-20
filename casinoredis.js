02f865be823efe3c07d92e66205bf05c8a3ee284
montenegrop <pablo.rpm@gmail.com>
2022-05-20 01:24:15 -0300

    fix

--- casinoredis.js
+++ casinoredis.js
@@ -4,14 +4,14 @@ const user = process.env.REDIS_CASINO_USER;
 const pass = process.env.REDIS_CASINO_PASSWORD;
 const customUrl = `redis://${user}:${pass}@redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817`;
 
-// const client = createClient({
-//   url: customUrl,
-// });
-
-const masterUrl = `redis://@redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817`;
 const client = createClient({
-  url: masterUrl,
-  password: process.env.MASTER_PASS,
+  url: customUrl,
 });
+
+// const masterUrl = `redis://@redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817`;
+// const client = createClient({
+//   url: masterUrl,
+//   password: process.env.MASTER_PASS,
+// });
 
 export default client;
